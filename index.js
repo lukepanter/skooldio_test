52b74c10736badded0ebb92502688a5d7ebc51cc
Jirawat Kusalangkurwat <ton_jirawat@live.com>
2023-02-24 20:55:46 +0700

    finish is card

--- index.js
+++ index.js
@@ -22,9 +22,34 @@ const shuffle = () => {
     }
     while(p);
     }
-
     return numbers;
-  };
+};
+
+const isCard = (numbers) => {
+    const suitDeck = ["spades","hearts","diamonds","clubs"]; 
+    const number = ["Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"];
+    let cards=[]
+    for (let i = 0; i < numbers.length; i++ ){
+        result = ""
+        num = numbers[i]%13
+        if (numbers[i] <= 13){
+            result = suitDeck[0] + "-" + number[num];
+        }else if (number[i] <= 26){
+            result = suitDeck[1] + "-" + number[num];
+        }else if (number[i] <= 39){
+            result = suitDeck[2] + "-" + number[num];
+        }else{
+            result = suitDeck[3] + "-" + number[num];
+        }
+        cards.push(result);
+    }
+    return cards
+};
+
+let numbers = shuffle();
+let cards = isCard(numbers);
+console.log(numbers)
+console.log(cards)
 
 
 
